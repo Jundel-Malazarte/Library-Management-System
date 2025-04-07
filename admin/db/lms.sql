c1815213fa60cde794dedc75f93ea11bf278dbe5
Jundel-Malazarte <jundelmalazarte348@gmail.com>
2025-04-07 09:45:07 +0800

    Updating the sql from the admin

---
+++ admin/db/lms.sql
@@ -0,0 +1,345 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 07, 2025 at 03:40 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `lms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `admin_id` int(11) NOT NULL,
+  `firstname` varchar(100) NOT NULL,
+  `middlename` varchar(100) NOT NULL,
+  `lastname` varchar(100) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `confirm_password` varchar(100) NOT NULL,
+  `admin_image` varchar(100) NOT NULL,
+  `admin_type` varchar(100) NOT NULL,
+  `admin_added` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`admin_id`, `firstname`, `middlename`, `lastname`, `username`, `password`, `confirm_password`, `admin_image`, `admin_type`, `admin_added`) VALUES
+(1, 'Admin', 'B.', 'User', 'admin', 'admin', 'admin', '', 'Admin', '2015-09-05 11:40:50'),
+(2, 'Jundel', 'B.', 'Malazarte', 'encoder', 'encoder', 'encoder', '', 'Encoder', '2015-09-29 11:40:50');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `allowed_book`
+--
+
+CREATE TABLE `allowed_book` (
+  `allowed_book_id` int(11) NOT NULL,
+  `qntty_books` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `allowed_days`
+--
+
+CREATE TABLE `allowed_days` (
+  `allowed_days_id` int(11) NOT NULL,
+  `no_of_days` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barcode`
+--
+
+CREATE TABLE `barcode` (
+  `barcode_id` int(11) NOT NULL,
+  `pre_barcode` varchar(100) NOT NULL,
+  `mid_barcode` int(100) NOT NULL,
+  `suf_barcode` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `borrow_book`
+--
+
+CREATE TABLE `borrow_book` (
+  `borrow_book_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `book_id` int(11) NOT NULL,
+  `date_borrowed` datetime NOT NULL,
+  `due_date` datetime NOT NULL,
+  `date_returned` datetime NOT NULL,
+  `borrowed_status` varchar(100) NOT NULL,
+  `book_penalty` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `category_id` int(11) NOT NULL,
+  `classname` varchar(50) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penalty`
+--
+
+CREATE TABLE `penalty` (
+  `penalty_id` int(11) NOT NULL,
+  `penalty_amount` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `report`
+--
+
+CREATE TABLE `report` (
+  `report_id` int(11) NOT NULL,
+  `book_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `admin_name` varchar(100) NOT NULL,
+  `detail_action` varchar(100) NOT NULL,
+  `date_transaction` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `return_book`
+--
+
+CREATE TABLE `return_book` (
+  `return_book_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `book_id` int(11) NOT NULL,
+  `date_borrowed` datetime NOT NULL,
+  `due_date` datetime NOT NULL,
+  `date_returned` datetime NOT NULL,
+  `book_penalty` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `school_number` varchar(100) NOT NULL,
+  `firstname` varchar(100) NOT NULL,
+  `middlename` varchar(100) NOT NULL,
+  `lastname` varchar(100) NOT NULL,
+  `contact` varchar(100) NOT NULL,
+  `gender` varchar(100) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `type` varchar(100) NOT NULL,
+  `level` varchar(100) NOT NULL,
+  `section` varchar(100) NOT NULL,
+  `user_image` varchar(100) NOT NULL,
+  `status` varchar(100) NOT NULL,
+  `user_added` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `school_number`, `firstname`, `middlename`, `lastname`, `contact`, `gender`, `address`, `type`, `level`, `section`, `user_image`, `status`, `user_added`) VALUES
+(42, '21429212', 'Ryan', '', 'Gabutin', '09123456789', 'Male', 'Guadalupe Cebu', 'Student', 'Fourt year', '4C', '', 'Active', '2025-04-07 09:30:44'),
+(43, '21536214', 'Jundel', 'B.', 'Malazarte', '09816650907', 'Male', 'Sambag 1 pob Cebu City', 'Student', 'Fourth year', '4C', '', 'Active', '2025-04-07 09:33:10');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_log`
+--
+
+CREATE TABLE `user_log` (
+  `user_log_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `user_name` varchar(100) NOT NULL,
+  `date_log` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `allowed_book`
+--
+ALTER TABLE `allowed_book`
+  ADD PRIMARY KEY (`allowed_book_id`);
+
+--
+-- Indexes for table `allowed_days`
+--
+ALTER TABLE `allowed_days`
+  ADD PRIMARY KEY (`allowed_days_id`);
+
+--
+-- Indexes for table `barcode`
+--
+ALTER TABLE `barcode`
+  ADD PRIMARY KEY (`barcode_id`);
+
+--
+-- Indexes for table `borrow_book`
+--
+ALTER TABLE `borrow_book`
+  ADD PRIMARY KEY (`borrow_book_id`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`category_id`),
+  ADD UNIQUE KEY `category_id` (`category_id`),
+  ADD KEY `classid` (`category_id`);
+
+--
+-- Indexes for table `penalty`
+--
+ALTER TABLE `penalty`
+  ADD PRIMARY KEY (`penalty_id`);
+
+--
+-- Indexes for table `report`
+--
+ALTER TABLE `report`
+  ADD PRIMARY KEY (`report_id`);
+
+--
+-- Indexes for table `return_book`
+--
+ALTER TABLE `return_book`
+  ADD PRIMARY KEY (`return_book_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `user_log`
+--
+ALTER TABLE `user_log`
+  ADD PRIMARY KEY (`user_log_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `allowed_book`
+--
+ALTER TABLE `allowed_book`
+  MODIFY `allowed_book_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `allowed_days`
+--
+ALTER TABLE `allowed_days`
+  MODIFY `allowed_days_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `barcode`
+--
+ALTER TABLE `barcode`
+  MODIFY `barcode_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `borrow_book`
+--
+ALTER TABLE `borrow_book`
+  MODIFY `borrow_book_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `penalty`
+--
+ALTER TABLE `penalty`
+  MODIFY `penalty_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `report`
+--
+ALTER TABLE `report`
+  MODIFY `report_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `return_book`
+--
+ALTER TABLE `return_book`
+  MODIFY `return_book_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+
+--
+-- AUTO_INCREMENT for table `user_log`
+--
+ALTER TABLE `user_log`
+  MODIFY `user_log_id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
